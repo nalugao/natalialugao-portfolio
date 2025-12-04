db6424e3f5391576178fe23890c3b1c9a50824c8
nalugao <nalugao@hotmail.com>
2025-12-04 01:02:15 -0300

    fix: atualizando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
 <footer className="text-center text-sm text-gray-500 mt-20">
-          Criado por Yudi Ganeko usando NextJs e TailwindCSS.
+          Criado por Natalia usando NextJs, React e CSS.
           <br /> © Todos os direitos reservados.
         </footer>
